9870d79791c7a188dbd51f4ba988b05315a38a6c
Rümeysa <ryavuz@avans.nl>
2020-02-04 21:00:55 +0100

    Opstart

---
+++ AVR_project/AVR_project/main.c
@@ -0,0 +1,18 @@
+/*
+ * AVR_project.c
+ *
+ * Created: 4-2-2020 20:42:58
+ * Author : Rumeysa
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
