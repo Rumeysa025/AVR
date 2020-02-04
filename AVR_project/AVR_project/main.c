cc2150f6cbdb7b209782c947f93b732ec0ed2745
Rümeysa <ryavuz@avans.nl>
2020-02-04 21:17:32 +0100

    Coding LED patterns using lookup

--- AVR_project/AVR_project/main.c
+++ AVR_project/AVR_project/main.c
@@ -38,9 +38,49 @@ void ButtonC0Pressed()
 	}
 }
 
+typedef struct {
+	unsigned char data;
+	unsigned int delay ;
+} PATTERN_STRUCT;
+
+PATTERN_STRUCT pattern[] = {
+	{0x00, 100}, {0x01, 100}, {0x02, 100}, {0x04, 100}, {0x08, 100}, {0x10, 100}, {0x20, 100}, {0x40, 100}, {0x80, 100},
+	{0x80, 200}, {0x40, 200}, {0x20, 200}, {0x10, 200}, {0x08, 100} , {0x04, 200}, {0x02, 200}, {0x01, 200}, {0x00, 200},
+	{0x00, 100},
+	{0xAA,  70}, {0x55,  50},
+	{0xAA,  70}, {0x55,  50},
+	{0xAA,  70}, {0x55,  50},
+	{0x00, 100},
+	{0x03, 100}, {0x06, 100}, {0x0c, 100}, {0x18, 100}, {0x30, 100}, {0x70, 100}, {0b11000000, 100},
+	
+	
+	{0x81, 100}, {0x42, 100}, {0x24, 100}, {0x18, 100}, {0x0F, 200}, {0xF0, 200}, {0x0F, 200}, {0xF0, 200},
+	{0x00, 0x00}
+};
+
+
 int main(void)
 {
-	/* Part B4 */
+		DDRD = 0b11111111;					// PORTD all output
+		
+		while (1)
+		{
+			// Set index to begin of pattern array
+			int index = 0;
+			// as long as delay has meaningful content
+			while( pattern[index].delay != 0 ) {
+				// Write data to PORTD
+				PORTD = pattern[index].data;
+				// wait
+				wait(pattern[index].delay);
+				// increment for next round
+				index++;
+			}
+		}
+
+	
+	/* 
+	---- Part B4 ----
 	DDRE = 0xff;
 	int i = 0;
 	while(1)
@@ -49,11 +89,11 @@ int main(void)
 		i = (i+1)% 8;
 		wait(50);
 	}
-	
-	/* Part B3 */
+	 
+	---- Part B3 ----
 	ButtonC0Pressed();
-	
-	/* Part B2 */
+		
+	---- Part B2 ---- 
 	DDRD = 0b11111111;			// All pins PORTD are set to output
 	DDRC = 0xff;
 	while (1)
@@ -67,8 +107,7 @@ int main(void)
 		wait( 500 );
 	}
 
-	/* Part A
-	
+	---- Part A ----	
 	DDRD = 0b11111111;
 	while (1)
 	{
@@ -79,7 +118,6 @@ int main(void)
 	}
 	
 	return 1;
-	
 	*/
 }
 
