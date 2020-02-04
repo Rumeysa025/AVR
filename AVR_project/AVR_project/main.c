5a6c50e972ae7e8d15eb1b9240c4a89c2dbf8fe5
Rümeysa <ryavuz@avans.nl>
2020-02-04 21:09:25 +0100

    B2 Coding I/O (easy)

--- AVR_project/AVR_project/main.c
+++ AVR_project/AVR_project/main.c
@@ -1,18 +1,63 @@
-/*
- * AVR_project.c
- *
- * Created: 4-2-2020 20:42:58
- * Author : Rumeysa
- */ 
-
 #include <avr/io.h>
+#include <util/delay.h>
 
+
+/******************************************************************/
+void wait( int ms )
+/* 
+short:			Busy wait number of millisecs
+inputs:			int ms (Number of millisecs to busy wait)
+outputs:	
+notes:			Busy wait, not very accurate. Make sure (external)
+				clock value is set. This is used by _delay_ms inside
+				util/delay.h
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+/******************************************************************/
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	/* Part B2 */
+	DDRD = 0b11111111;			// All pins PORTD are set to output
+	DDRC = 0xff;
+	while (1)
+	{
+		//PORTD = 0xAA;			// Write 10101010b PORTD
+		PORTC = 0b10000000;
+		
+		wait( 500 );
+		//PORTD = 0x55;			// Write 01010101b PORTD
+		PORTC = 0b01000000;
+		wait( 500 );
+	}
+
+	/* Part A
+	
+	DDRD = 0b11111111;
+	while (1)
+	{
+		PORTD = 0xAA;
+		wait( 250 );
+		PORTD = 0x55;			// Write 01010101b PORTD
+		wait( 250 );
+	}
+	
+	return 1;
+	
+	*/
 }
+
+
+
+
+
+
+
 
